1ead61e283b9cfaa262a48fc0e85f90a30a03972
Felipe Otelakoski <felipe@agenciasimetria.com.br>
2021-06-06 09:30:17 -0300

    pg login

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import stylesLogin from '../styles/login/login.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      Hello Mundo
-    </div>
+
+    <div id={stylesLogin.pageLogin}>
+
+      <div className={stylesLogin.BoxLogin}>        
+          <img src="/logo-relatou-branco.svg" alt="Faça seu Login!" />
+          <form action="/dash" method="">
+            <input type="text" name="nome" id="nome" placeholder="Login" />
+            <input type="password" name="senha" id="senha" placeholder="Senha" />
+            <button>Fazer <strong>Login</strong></button>
+          </form>
+      </div>
+
+    </div>    
+
   )
 }
